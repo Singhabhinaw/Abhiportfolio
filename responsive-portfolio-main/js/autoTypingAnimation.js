8c51dd37f79da0a156dee2e055f03896bb158f51
Abhinaw <abhinawsingh620@gmail.com>
2024-11-26 13:24:51 +0530

    first commit

---
+++ responsive-portfolio-main/js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Abhinaw Singh</span> Frontend Developer with hands-on experience in HTML, CSS, and JavaScript, creating responsive and user-friendly web applications. Proficient in Java programming for application development and problem-solving. Passionate about leveraging both frontend and programming skills to create innovative solutions, while currently expanding knowledge in AI through Samsung Innovation Campus training."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
